85ed5ae01d5f6a60674985a87abec3597b33e3ab
Zhangpeir <3955159411@qq.com>
2026-06-04 11:50:49 +0800

    活动状态修复null值问题

--- java-server/src/main/java/cn/edu/sdu/java/server/services/ActivityService.java
+++ java-server/src/main/java/cn/edu/sdu/java/server/services/ActivityService.java
@@ -62,6 +62,8 @@ public class ActivityService {
             activity.setEndTime(endTime);
             activity.setDescription(description);
             activity.setPublisherId(CommonMethod.getPersonId());
+            // 设置默认状态:0=未开始
+            activity.setStatus(0);
             activity.setCreateTime(new Date());
 
             activityRepository.save(activity);
